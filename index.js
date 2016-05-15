5ac306c6e39da37f3a78ecf24b0e787fdfe583a9
manthoshkumar.t <manthoshkumar.t@zohocorp.com>
2016-05-15 16:45:40 +0530

    Changed to messenger-bot module

--- index.js
+++ index.js
@@ -1,53 +1,28 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
+'use strict'
+const http = require('http')
+const Bot = require('messenger-bot')
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+let bot = new Bot({
+  token: process.env.PAGE_ACCESS_TOKEN,
+  verify: 'testbot_verify_token'
+})
 
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
-});
+bot.on('error', (err) => {
+  console.log(err.message)
+})
 
-// Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
-});
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text
 
-// handler receiving messages
-app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-        	console.log(`Data : ${event.sender.id} :: ${event.message.text}`);
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
-    }
-    res.sendStatus(200);
-});
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
 
-function sendMessage(recipientId, message) {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};
+    reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+})
+
+http.createServer(bot.middleware()).listen(process.env.PORT || 3000)
