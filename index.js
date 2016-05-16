43c76167a2ed52a349f6dd883c67ef0524e1880d
manthoshkumar.t <manthoshkumar.t@zohocorp.com>
2016-05-16 15:56:19 +0530

    Checked for the command @goto and called Google API

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict'
-const http = require('http')
-const Bot = require('messenger-bot')
+const http = require('http');
+const request = require('request');
+const Bot = require('messenger-bot');
+
+const GOTO_COMMAND = "@goto";
+const BASE_URI = "https://maps.googleapis.com/maps/api/distancematrix/json?";
 
 let bot = new Bot({
   token: process.env.PAGE_ACCESS_TOKEN,
   verify: 'testbot_verify_token'
-})
+});
 
 bot.on('error', (err) => {
-  console.log(err.message)
-})
+  console.log(err.message);
+});
 
 bot.on('message', (payload, reply) => {
-  let text = payload.message.text
+  let text = payload.message.text;
 
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) {
@@ -20,18 +24,38 @@ bot.on('message', (payload, reply) => {
         // throw err
     }
     else {
-        text = profile.first_name+"::"+ text ;
-        reply({text}, (err) => {
-          if (err) {
-            console.log(err);
-            // throw err
+        let commands = text.split('/');
+        if(commands.length >= 1 && commands[0] === GOTO_COMMAND) {
+            let url = `${BASE_URI}origins=${commands[1]}&destinations=${commands[2]}&mode=driving&departure_time=now&key=${process.env.GOOGLE_API_TOKEN}`;
+            request(encodeURIComponent(url), function (error, response, body) {
+                  if (!error && response.statusCode == 200) {
+                        console.log(`Google response : ${body}`) // Show the HTML for the Google homepage.
+                        reply({text}, (err) => {
+                            if (err) {
+                                console.log(err);
+                                // throw err
+                            }
+                            else {
+                              console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`);
+                            }
+                        });     
+                  }
+            })
         }
         else {
-          console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+            text = "That doesn't ring a bell. Try @goto/<source>/<dest>";
+            reply({text}, (err) => {
+                if (err) {
+                    console.log(err);
+                    // throw err
+                }
+                else {
+                  console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`);
+                }
+            });
         }
-        })
     }
-  })
-})
+  });
+});
 
-http.createServer(bot.middleware()).listen(process.env.PORT || 3000)
+http.createServer(bot.middleware()).listen(process.env.PORT || 3000);
